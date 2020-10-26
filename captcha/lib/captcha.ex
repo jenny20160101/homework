1f7b99ace565d8ff7e6f6902d43e7f25c1534826
huzeli <jenny.hu@scrumcn.com>
2020-10-27 07:10:50 +0800

    step 2-with one time

--- captcha/lib/captcha.ex
+++ captcha/lib/captcha.ex
@@ -4,18 +4,24 @@ defmodule Captcha do
   """
 
   def new(phone, code) do
-    %{phone: phone, code: code}
+    %{phone: phone, code: code, remaining: 3}
   end
 
   def verify(%{phone: phone, code: code} = captcha, phone, code) do
-    {:ok, captcha}
+    new_captcha = decrease_remaining(captcha)
+    {:ok, new_captcha}
   end
 
   def verify(%{phone: phone} = captcha, phone, _code) do
-    {:mismatched, captcha}
+    new_captcha = decrease_remaining(captcha)
+    {:mismatched, new_captcha}
   end
 
   def verify(captcha, _phone, _code) do
     {:captcha_expired, captcha}
+  end
+
+  defp decrease_remaining(%{remaining: remaining} = captcha) do
+    %{captcha | remaining: remaining - 1}
   end
 end
--- captcha/test/captcha_test.exs
+++ captcha/test/captcha_test.exs
@@ -5,18 +5,18 @@ defmodule CaptchaTest do
   @code "1234"
 
   test "create captcha" do
-    assert %{phone: "1", code: "a"} = Captcha.new("1", "a")
+    assert %{phone: "1", code: "a", remaining: 3} = Captcha.new("1", "a")
     assert %{phone: "2", code: "b"} = Captcha.new("2", "b")
   end
 
+  defp with_captcha(_tags) do
+    captcha = Captcha.new(@phone, @code)
+    {:ok, captcha: captcha}
+  end
+
+  setup [:with_captcha]
+
   describe "verify" do
-    defp with_captcha(_tags) do
-      captcha = Captcha.new(@phone, @code)
-      {:ok, captcha: captcha}
-    end
-
-    setup [:with_captcha]
-
     test "should return :ok with matched phone and code", %{captcha: captcha} do
       assert {:ok, _captcha} = Captcha.verify(captcha, @phone, @code)
     end
@@ -27,6 +27,45 @@ defmodule CaptchaTest do
 
     test "should return :captcha_expired with mismatched phone", %{captcha: captcha} do
       assert {:captcha_expired, _captcha} = Captcha.verify(captcha, @phone <> "1", @code)
+    end
+  end
+
+  describe "verify with remaining" do
+    test "should return decreased remaining with matched phone and code", %{captcha: captcha} do
+      assert {:ok, %{remaining: 2} = _captcha} = Captcha.verify(captcha, @phone, @code)
+    end
+
+    test "should return decreased remaining with dismatched code", %{captcha: captcha} do
+      assert {:mismatched, %{remaining: 2} = _captcha} =
+               Captcha.verify(captcha, @phone, @code <> "1")
+    end
+
+    test "should not change remaining with dismatched phone", %{captcha: captcha} do
+      assert {:captcha_expired, %{remaining: 3} = _captcha} =
+               Captcha.verify(captcha, @phone <> "1", @code)
+    end
+  end
+
+  describe "verify with one time remaining" do
+    defp with_set_remaining_to_one(%{captcha: captcha}) do
+      new_captcha = %{captcha | remaining: 1}
+      {:ok, captcha: new_captcha}
+    end
+
+    setup [:with_set_remaining_to_one]
+
+    test "should return decreased remaining with matched phone and code", %{captcha: captcha} do
+      assert {:ok, %{remaining: 0} = _captcha} = Captcha.verify(captcha, @phone, @code)
+    end
+
+    test "should return decreased remaining with dismatched code", %{captcha: captcha} do
+      assert {:mismatched, %{remaining: 0} = _captcha} =
+               Captcha.verify(captcha, @phone, @code <> "1")
+    end
+
+    test "should not change remaining with dismatched phone", %{captcha: captcha} do
+      assert {:captcha_expired, %{remaining: 1} = _captcha} =
+               Captcha.verify(captcha, @phone <> "1", @code)
     end
   end
 end
