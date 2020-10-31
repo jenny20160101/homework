d6390ef10e17995ce0c5031448cf6e484bb026e5
huzeli <jenny.hu@scrumcn.com>
2020-10-31 22:18:41 +0800

    start concurrency

--- captcha/lib/captcha_handler.ex
+++ captcha/lib/captcha_handler.ex
@@ -1,12 +1,25 @@
 defmodule CaptchaHandler do
   use GenServer
   @impl true
-  def init(state) do
-    IO.inspect(state, label: "state:")
-    {:ok, state}
+  def init([phone, code]) do
+    {:ok, Captcha.new(phone, code)}
   end
 
-  def start_link(state)  do
+  def start_link(state) do
     GenServer.start_link(__MODULE__, state)
+  end
+
+  def verify(handler, phone, code) do
+    GenServer.call(handler, {:verify, phone, code})
+  end
+
+  @impl true
+  def handle_call({:verify, phone, code}, _from, captcha) do
+    #    IO.inspect(captcha, label: "captcha1:")
+    #    IO.inspect(phone, label: "phone1:")
+    #    IO.inspect(code, label: "code1:")
+
+    {result, new_captcha} = Captcha.verify(captcha, phone, code)
+    {:reply, result, new_captcha}
   end
 end
--- captcha/test/captcha_handler_test.exs
+++ captcha/test/captcha_handler_test.exs
@@ -4,18 +4,84 @@ defmodule CaptchaHandlerTest do
   @phone "18600000000"
   @code "1234"
 
-  test "spawn captcha handler" do
-#    {:ok, pid} = GenServer.start_link(CaptchaHandler, [@phone, @code])
-#    assert pid != nil
-#    start_supervised 建议在test中使用，test能控制 启动的其他进程的生命周期
-
+  setup do
     captchaHandler = start_supervised!({CaptchaHandler, [@phone, @code]})
-    IO.inspect(captchaHandler, label: "captchaHandler:")
+    {:ok, handler: captchaHandler}
+  end
+
+  test "spawn captcha handler", %{handler: captchaHandler} do
+    #    {:ok, pid} = GenServer.start_link(CaptchaHandler, [@phone, @code])
+    #    assert pid != nil
+    #    start_supervised 建议在test中使用，test能控制 启动的其他进程的生命周期
+
+    #    IO.inspect(captchaHandler, label: "captchaHandler3:")
     assert captchaHandler != nil
   end
 
   describe "sequential verify" do
-    test "return :ok with matched phone and code" do
+    test "return :ok with matched phone and code", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+    end
+
+    test "return :mismatched with mismatched code", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+    end
+
+    test "should return :captcha_expired with mismatched phone", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :captcha_expired = CaptchaHandler.verify(captchaHandler, @phone <> "1", @code)
+    end
+
+    test "still :ok with matched phone and code for 3 times", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+    end
+
+    test "still :mismatched with mismatched for 3 times", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+    end
+
+    test "3 :ok and 1 captcha_expired with matched phone and code", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+      assert :captcha_expired = CaptchaHandler.verify(captchaHandler, @phone, @code)
+    end
+
+    test "3 :mismatched and 1 captcha_expired with mismatched code", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+      assert :captcha_expired = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+    end
+
+    test "mixed cases", %{handler: captchaHandler} do
+      #      IO.inspect(captchaHandler, label: "captchaHandler2:")
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+      assert :captcha_expired = CaptchaHandler.verify(captchaHandler, @phone <> "1", @code)
+      assert :ok = CaptchaHandler.verify(captchaHandler, @phone, @code)
+      assert :mismatched = CaptchaHandler.verify(captchaHandler, @phone, @code <> "1")
+    end
+  end
+
+  describe "verify with concurrency" do
+    test "should got 3 :ok for 3 concurrency invoke", %{handler: captchaHandler} do
+      assert [:ok, :ok, :ok] ==
+               [
+                 Task.async(fn -> CaptchaHandler.verify(captchaHandler, @phone, @code) end),
+                 Task.async(fn -> CaptchaHandler.verify(captchaHandler, @phone, @code) end),
+                 Task.async(fn -> CaptchaHandler.verify(captchaHandler, @phone, @code) end)
+               ]
+               |> Task.await_many()
     end
   end
 end
