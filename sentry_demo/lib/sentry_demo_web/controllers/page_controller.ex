8d26954896277196c5561dd70fbfc67f8e609af2
jenny <jenny.hu@scrumcn.com>
2020-11-22 15:02:14 +0800

    sentry-demo

---
+++ sentry_demo/lib/sentry_demo_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule SentryDemoWeb.PageController do
+  use SentryDemoWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
---
+++ sentry_demo/test/sentry_demo_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule SentryDemoWeb.PageControllerTest do
+  use SentryDemoWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
