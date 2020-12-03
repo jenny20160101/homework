a9190b31dccd0bfb78f4820db054094cc99f40de
huzeli <jenny.hu@scrumcn.com>
2020-12-03 22:24:02 +0800

    puzzel2

--- puzzle/lib/puzzle2.ex
+++ puzzle/lib/puzzle2.ex
@@ -3,31 +3,32 @@ defmodule Puzzle2 do
   Documentation for `Puzzle`.
   """
 
-  def convert_input_to_list() do
+  def check_pwd_in_file() do
     {:ok, file_content} = File.read("/data/homework/puzzle/lib/puzzle2_input.txt")
+#    IO.inspect(file_content, Label: "file_content:", pretty: true)
 
     # 字符串转换为 list
-    pwd_list1 = String.split(file_content)
+    pwd_list1 = String.split(file_content, "\n")
+#    IO.inspect(pwd_list1, Label: "pwd_list1:", pretty: true)
 
     # 将list中的每一项 改为key value 3-4 b: lbbbbntqswsv
-    pwd_list2 =
-      Enum.map(pwd_list1, convert_line_content(line))
-
-    pwd_list2
+    Enum.map(pwd_list1, fn x -> convert_line_content(x) end)
+#    |> IO.inspect(Label: "list:", pretty: true)
+    |> valid_pwd_count()
   end
 
   # line的格式：3-4 b: lbbbbntqswsv
   def convert_line_content(line) do
     [times | [find_char, pwd]] = String.split(line)
-#    IO.inspect(times, Lable: "times", pretty: true)
-#    IO.inspect(find_char, Lable: "find_char", pretty: true)
-#    IO.inspect(pwd, Lable: "pwd", pretty: true)
+    #    IO.inspect(times, Lable: "times", pretty: true)
+    #    IO.inspect(find_char, Lable: "find_char", pretty: true)
+    #    IO.inspect(pwd, Lable: "pwd", pretty: true)
 
     times_list = String.split(times, "-")
     from_times = List.first(times_list)
     to_times = List.last(times_list)
-#    IO.inspect(from_times, Lable: "from_times", pretty: true)
-#    IO.inspect(to_times, Lable: "to_times", pretty: true)
+    #    IO.inspect(from_times, Lable: "from_times", pretty: true)
+    #    IO.inspect(to_times, Lable: "to_times", pretty: true)
 
     %{
       from_times: String.to_integer(from_times),
@@ -35,5 +36,24 @@ defmodule Puzzle2 do
       find_char: String.trim(find_char, ":"),
       pwd: pwd
     }
+  end
+
+  def find_char_count(subject, find_char) do
+    subject_length_exclude_find_char = String.replace(subject, find_char, "") |> String.length()
+    String.length(subject) - subject_length_exclude_find_char
+  end
+
+  def check_pwd(%{
+        from_times: from_times,
+        to_times: to_times,
+        find_char: find_char,
+        pwd: pwd
+      }) do
+    count = find_char_count(pwd, find_char)
+    count >= from_times && count <= to_times
+  end
+
+  def valid_pwd_count(input_list) do
+    Enum.filter(input_list, fn x -> check_pwd(x) == true end) |> Enum.count()
   end
 end
--- puzzle/test/puzzle2_test.exs
+++ puzzle/test/puzzle2_test.exs
@@ -12,4 +12,91 @@ defmodule Puzzle2Test do
              pwd: "lbbbbntqswsv"
            }
   end
+
+  test "find char times" do
+    assert Puzzle2.find_char_count("abcdefg", "a") == 1
+    assert Puzzle2.find_char_count("bcdefg", "a") == 0
+    assert Puzzle2.find_char_count("aaabcdefg", "a") == 3
+    assert Puzzle2.find_char_count("acacabcdefg", "a") == 3
+    assert Puzzle2.find_char_count("cbcdefgaaa", "a") == 3
+    assert Puzzle2.find_char_count("cbcdeddssaaafgaaa", "a") == 6
+  end
+
+  test "check char times" do
+    assert Puzzle2.check_pwd(%{
+             from_times: 1,
+             to_times: 3,
+             find_char: "b",
+             pwd: "lbbbbntqswsv"
+           }) == false
+
+    assert Puzzle2.check_pwd(%{
+             from_times: 1,
+             to_times: 4,
+             find_char: "b",
+             pwd: "lbbbbntqswsv"
+           }) == true
+
+    assert Puzzle2.check_pwd(%{
+             from_times: 4,
+             to_times: 10,
+             find_char: "b",
+             pwd: "lbbbbntqswsv"
+           }) == true
+
+    assert Puzzle2.check_pwd(%{
+             from_times: 4,
+             to_times: 4,
+             find_char: "b",
+             pwd: "lbbbbntqswsv"
+           }) == true
+
+    assert Puzzle2.check_pwd(%{
+             from_times: 5,
+             to_times: 10,
+             find_char: "b",
+             pwd: "lbbbbntqswsv"
+           }) == false
+  end
+
+  test "valid pwd count" do
+    list = [
+      %{
+        from_times: 1,
+        to_times: 3,
+        find_char: "b",
+        pwd: "lbbbbntqswsv"
+      },
+      %{
+        from_times: 1,
+        to_times: 4,
+        find_char: "b",
+        pwd: "lbbbbntqswsv"
+      },
+      %{
+        from_times: 4,
+        to_times: 10,
+        find_char: "b",
+        pwd: "lbbbbntqswsv"
+      },
+      %{
+        from_times: 4,
+        to_times: 4,
+        find_char: "b",
+        pwd: "lbbbbntqswsv"
+      },
+      %{
+        from_times: 5,
+        to_times: 10,
+        find_char: "b",
+        pwd: "lbbbbntqswsv"
+      }
+    ]
+
+    assert Puzzle2.valid_pwd_count(list) == 3
+  end
+
+  test "check input file" do
+    assert Puzzle2.check_pwd_in_file == 586
+  end
 end
