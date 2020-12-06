237d7b10e1c9fd57f5120dd834bda17fd9c21127
huzeli <jenny.hu@scrumcn.com>
2020-12-06 15:36:05 +0800

    day3finish

--- puzzle/lib/puzzle3.ex
+++ puzzle/lib/puzzle3.ex
@@ -2,27 +2,26 @@ defmodule Puzzle3 do
   @moduledoc """
   Documentation for `Puzzle`.
   """
-  @line_length 66
+  @line_length 31
 
-  def count_tree1() do
+  def count_tree() do
     {:ok, file_content} = File.read("/data/homework/puzzle/lib/puzzle3_input.txt")
     #    #IO.inspect(file_content, Label: "file_content:", pretty: true)
 
     # 字符串转换为 list
-    pwd_list1 = String.split(file_content, "\n")
+    map_list = convert_input_file_to_list(file_content)
     #    #IO.inspect(pwd_list1, Label: "pwd_list1:", pretty: true)
 
     #    # 将list中的每一项 改为key value 3-4 b: lbbbbntqswsv
-    #    Enum.map(pwd_list1, fn line -> convert_line_content(line) end)
-    #    #    |> #IO.inspect(Label: "list:", pretty: true)
-    #    |> valid_pwd_count()
+
+    tree_count_in_trace( map_list)
   end
 
   def get_trace(trace, map_list) do
     %{line: line, column: column} = List.last(trace)
 
     result = right_3_and_down_1(line, column)
-    IO.inspect(result, label: "result1", pretty: true)
+    #    IO.inspect(result, label: "result1", pretty: true)
 
     if result == :finished do
       trace
@@ -45,15 +44,15 @@ defmodule Puzzle3 do
     %{line: line, column: new_column}
   end
 
-  def adjust_column_when_exceed(column) when column <= 66 do
+  def adjust_column_when_exceed(column) when column <= 31 do
     column
   end
 
-  def adjust_column_when_exceed(column) when column > 66 do
+  def adjust_column_when_exceed(column) when column > 31 do
     Integer.mod(column, @line_length)
   end
 
-  def down_one(line, _column) when line == 11 do
+  def down_one(line, _column) when line == 323 do
     :finished
   end
 
@@ -66,14 +65,22 @@ defmodule Puzzle3 do
     line_content = Enum.at(map_list, line - 1)
 
     # IO.inspect(line_content, label: "is_tree_at_coordinate:line_content", pretty: true)
-#    IO.inspect(line, label: "line", pretty: true)
-if String.slice(line_content, column - 1, 1) == "#" do
-    IO.inspect("#{line}-#{column}", label: "line:column", pretty: true)
-end
-#     IO.inspect(String.slice(line_content, column - 1, 1),
-#          label: "is_tree_at_coordinate:char",
-#          pretty: true
-#        )
+    IO.inspect(line_content, label: "查看坐标数据，坐标数据，line_content", pretty: true)
+    IO.inspect("#{line}-#{column}", label: "查看坐标数据，坐标", pretty: true)
+
+#    IO.inspect(Enum.count(map_list),
+#      label: "is_tree_at_coordinate:Enum.count(map_list)",
+#      pretty: true
+#    )
+
+    if String.slice(line_content, column - 1, 1) == "#" do
+      IO.inspect("#{line}-#{column}", label: "line:column", pretty: true)
+    end
+
+    #     IO.inspect(String.slice(line_content, column - 1, 1),
+    #          label: "is_tree_at_coordinate:char",
+    #          pretty: true
+    #        )
 
     String.slice(line_content, column - 1, 1) == "#"
   end
@@ -86,8 +93,14 @@ end
     down_one(line, column)
   end
 
-  def trees_in_trace(trace, map_list) do
-    Enum.filter(trace, fn %{line: line, column: column} ->
+  def tree_count_in_trace(map_list) do
+
+    trace = get_trace([%{line: 1, column: 1}], map_list)
+    IO.inspect(trace, label: "trace", pretty: true)
+
+
+
+    Enum.filter(trace, fn %{line: line, column: column} = _ ->
       is_tree_at_coordinate(map_list, line, column)
     end)
     |> Enum.count()
--- puzzle/test/puzzle3_test.exs
+++ puzzle/test/puzzle3_test.exs
@@ -13,8 +13,8 @@ defmodule Puzzle2Test do
 #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
 #...##....##...##....##...##....##...##....##...##....##...##....#
 .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
-  @line_length_sample 66
-  @line_count_sample 11
+  @line_length_sample 31
+  @line_count_sample 323
 
   test "convert input file" do
     input_content = "........#....#..##..#...#.....#
@@ -60,7 +60,7 @@ defmodule Puzzle2Test do
   end
 
   test "right three 不会超过右边边界" do
-    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+    #    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
 
     assert Puzzle3.right_three(1, 1) == %{line: 1, column: 4}
 
@@ -74,7 +74,7 @@ defmodule Puzzle2Test do
   end
 
   test "right three 会超过右边边界" do
-    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+    #    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
 
     assert Puzzle3.right_three(1, @line_length_sample - 3) ==
              %{
@@ -102,7 +102,7 @@ defmodule Puzzle2Test do
   end
 
   test "down 1" do
-    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+    #    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
     assert Puzzle3.down_one(1, 3) == %{line: 2, column: 3}
     assert Puzzle3.down_one(2, 7) == %{line: 3, column: 7}
   end
@@ -113,44 +113,47 @@ defmodule Puzzle2Test do
   end
 
   test "right 3 down 1" do
-    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+    #    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
 
     assert Puzzle3.right_3_and_down_1(1, 1) ==
              %{line: 2, column: 4}
-
 
     assert Puzzle3.right_3_and_down_1(10, 1) ==
              %{line: 11, column: 4}
   end
 
-    test "right 3：超过右边界，则地图向右复制一倍。可以使用 取模，重新回到第一列" do
-      map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+  test "right 3：超过右边界，则地图向右复制一倍。可以使用 取模，重新回到第一列" do
+    #      map_list = Puzzle3.convert_input_file_to_list(@input_sample)
 
-      assert Puzzle3.right_3_and_down_1(1, @line_length_sample - 2) == %{
-               line: 2,
-               column: 1
-             }
+    assert Puzzle3.right_3_and_down_1(1, @line_length_sample - 2) == %{
+             line: 2,
+             column: 1
+           }
 
-      assert Puzzle3.right_3_and_down_1(1, @line_length_sample - 1) == %{
-               line: 2,
-               column: 2
-             }
+    assert Puzzle3.right_3_and_down_1(1, @line_length_sample - 1) == %{
+             line: 2,
+             column: 2
+           }
 
-      assert Puzzle3.right_3_and_down_1(1, @line_length_sample) == %{
-               line: 2,
-               column: 3
-             }
-    end
+    assert Puzzle3.right_3_and_down_1(1, @line_length_sample) == %{
+             line: 2,
+             column: 3
+           }
+  end
 
   test "get_trace" do
     map_list = Puzzle3.convert_input_file_to_list(@input_sample)
     trace = Puzzle3.get_trace([%{line: 1, column: 1}], map_list)
-    assert Enum.count(trace) == 11
+    assert Enum.count(trace) == @line_count_sample
   end
 
+  #  test "trees in trace  --sample" do
+  #    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+  #    trace = Puzzle3.get_trace([%{line: 1, column: 1}], map_list)
+  #    assert Puzzle3.tree_count_in_trace(trace, map_list) == 7
+  #  end
+
   test "trees in trace" do
-    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
-    trace = Puzzle3.get_trace([%{line: 1, column: 1}], map_list)
-    assert Puzzle3.trees_in_trace(trace, map_list) == 7
+    assert Puzzle3.count_tree() == 153
   end
 end
