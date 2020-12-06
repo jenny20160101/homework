4655689ecabb2c858d627123e2c8501b99af38b8
huzeli <jenny.hu@scrumcn.com>
2020-12-06 21:08:19 +0800

    简化代码

--- puzzle/lib/puzzle5.ex
+++ puzzle/lib/puzzle5.ex
@@ -7,17 +7,19 @@ defmodule Puzzle5 do
     chars = String.split(boarding_pass, "", trim: true)
     chars_of_row = Enum.take(chars, 7)
     range = %{start: 0, end1: 127}
-
-    range = get_half_region_of_row(Enum.at(chars_of_row, 0), range)
-    range = get_half_region_of_row(Enum.at(chars_of_row, 1), range)
-    range = get_half_region_of_row(Enum.at(chars_of_row, 2), range)
-    range = get_half_region_of_row(Enum.at(chars_of_row, 3), range)
-    range = get_half_region_of_row(Enum.at(chars_of_row, 4), range)
-    range = get_half_region_of_row(Enum.at(chars_of_row, 5), range)
-    range = get_half_region_of_row(Enum.at(chars_of_row, 6), range)
-
-    %{start: start, end1: end1} = range
+    %{start: start, end1: end1} = get_half_region_of_rows(chars_of_row, 0, range)
     start
+  end
+
+  def get_half_region_of_rows(chars_of_row, index, %{start: start, end1: end1} = range)
+      when index <= 6 do
+    range = get_half_region_of_row(Enum.at(chars_of_row, index), range)
+    get_half_region_of_rows(chars_of_row, index + 1, range)
+  end
+
+  def get_half_region_of_rows(chars_of_row, index, %{start: start, end1: end1} = range)
+      when index > 6 do
+    range
   end
 
   def column(boarding_pass) do
@@ -47,16 +49,12 @@ defmodule Puzzle5 do
     %{start: start, end1: new_end}
   end
 
-  def get_half_region_of_row(char, %{start: start, end1: end1} = range) when start == end1 do
-    range
-  end
-
-  def get_half_region_of_row("B", %{start: start, end1: end1} = range) when start != end1 do
+  def get_half_region_of_row("B", %{start: start, end1: end1} = range) do
     new_start = start + Integer.floor_div(end1 - start, 2) + 1
     %{start: new_start, end1: end1}
   end
 
-  def get_half_region_of_row("F", %{start: start, end1: end1} = range) when start != end1 do
+  def get_half_region_of_row("F", %{start: start, end1: end1} = range) do
     new_end = start + Integer.floor_div(end1 - start, 2)
     %{start: start, end1: new_end}
   end
