c32f2f75e316991165fba06332150b787951cf9c
huzeli <jenny.hu@scrumcn.com>
2020-12-06 17:09:18 +0800

    day4-part1-finish

---
+++ puzzle/lib/puzzle4.ex
@@ -0,0 +1,29 @@
+defmodule Puzzle4 do
+  @moduledoc """
+  Documentation for `Puzzle`.
+  """
+
+  def is_valid_passport(passport_string) do
+    property_list = String.split(passport_string)
+    property_count = Enum.count(property_list)
+
+    case property_count do
+      8 -> true
+      7 -> only_lack_cid(property_list)
+      _ -> false
+    end
+  end
+
+  def only_lack_cid(property_list) do
+    !Enum.any?(property_list, fn x -> String.match?(x, ~r/cid/) end)
+  end
+
+  def count_valid_passport(file_path)
+    do
+    {:ok, file_content} = File.read(file_path)
+
+    passport_list = String.split(file_content, "\n\n")
+
+    Enum.filter(passport_list, fn x-> is_valid_passport(x) end) |> Enum.count()
+  end
+end
---
+++ puzzle/test/puzzle4_test.exs
@@ -0,0 +1,73 @@
+defmodule Puzzle4Test do
+  use ExUnit.Case
+  doctest Puzzle4
+
+  #  @input_sample "..##.........##.........##.........##.........##.........##.......
+  ## ...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+  # .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+  # ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+  # .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+  # ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
+  # .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+  # .#........#.#........#.#........#.#........#.#........#.#........#
+  ## .##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+  ## ...##....##...##....##...##....##...##....##...##....##...##....#
+  # .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
+  #  @line_length 31
+  #  @line_count 323
+
+  #  test "convert input file" do
+  #    input_content = "........#....#..##..#...#.....#
+  # ...............#....##........#
+  # .#....##...##..#...............
+  # .#.......#......#.##..##...#..."
+  #
+  #    assert Puzzle3.convert_input_file_to_list(input_content) == [
+  #             "........#....#..##..#...#.....#",
+  #             "...............#....##........#",
+  #             ".#....##...##..#...............",
+  #             ".#.......#......#.##..##...#..."
+  #           ]
+  #  end
+  #
+  #  test "找到正确的行" do
+  #    map_list = Puzzle3.convert_input_file_to_list(@input_sample)
+  #
+  #    assert Puzzle3.line(map_list, 1) ==
+  #             "..##.........##.........##.........##.........##.........##......."
+  #
+  #    assert Puzzle3.line(map_list, 2) ==
+  #             "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#.."
+  #
+  #    assert Puzzle3.line(map_list, 3) ==
+  #             ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#."
+  #  end
+
+  test "包含8个属性，则 valid" do
+    assert Puzzle4.is_valid_passport("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm") == true
+  end
+
+  test "仅仅少了cid，则 valid" do
+    assert Puzzle4.is_valid_passport("hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm") == true
+  end
+
+  test "缺少的属性，除了cid还有其他的，则 invalid" do
+    assert Puzzle4.is_valid_passport("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929") == false
+    assert Puzzle4.is_valid_passport("hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in") == false
+  end
+
+  test "整个文档中 valid passport数量-sample" do
+    assert Puzzle4.count_valid_passport("/data/homework/puzzle/lib/puzzle4_input_sample.txt") == 2
+  end
+
+
+  test "整个文档中 valid passport数量" do
+    assert Puzzle4.count_valid_passport("/data/homework/puzzle/lib/puzzle4_input.txt") == 182
+  end
+end
