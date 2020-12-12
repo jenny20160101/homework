5fac74f047e0bf92b288b954c9b5fcf194010f7b
huzeli <jenny.hu@scrumcn.com>
2020-12-12 21:15:31 +0800

    day7-part1 速度太慢，废弃

--- puzzle/lib/puzzle7Part1.ex
+++ puzzle/lib/puzzle7Part1.ex
@@ -1,73 +1,73 @@
-defmodule Puzzle7Part1 do
-  @moduledoc """
-  Documentation for `Puzzle`.
-  """
-
-  def contain_bag_directly(line, color) do
-    {:ok, reg} = Regex.compile("^(?!" <> color <> ")(.*)" <> color)
-    String.match?(line, reg)
-  end
-
-  def contain_bag(line, color, [_ | _] = all_lines) do
-    #    IO.inspect(line, label: "line", pretty: true)
-
-    if contain_bag_directly(line, color) do
-      #      IO.inspect(color, label: "contain_bag_directly", pretty: true)
-      true
-    else
-      #       找到 line 包含的所有儿子 ，check儿子是否包含color
-      extract_contained_colors(line)
-      |> IO.inspect(label: "extract_contained_colors", pretty: true)
-      |> Enum.map(fn x_color -> find_color_rule_line(x_color, all_lines) end)
-      |> IO.inspect(label: "find_color_rule_line ", pretty: true)
-      |> Enum.any?(fn x_line -> contain_bag(x_line, color, all_lines) end)
-    end
-  end
-
-  def count_contain_bag([_ | _] = lines, color) do
-    Enum.filter(lines, fn x_line -> contain_bag(x_line, color, lines) end)
-    #    |> IO.inspect(label: "count_contain_bag", pretty: true)
-    |> Enum.count()
-  end
-
-  def count_contain_bag(file_path, color) do
-    {:ok, file_content} = File.read(file_path)
-
-    String.split(file_content, "\n")
-    |> count_contain_bag(color)
-  end
-
-  def extract_contained_colors(line) do
-    [_head | tail] = Regex.split(~r{(contain|,|\.)}, line, trim: true)
-
-    color_list =
-      tail
-      #      |> IO.inspect(label: "extract_contained_colors1", pretty: true)
-      |> Enum.map(fn x ->
-        color = Regex.run(~r/\d+ (.+bag)/, x)
-        #          |> IO.inspect(label: "extract_contained_colors2", pretty: true)
-
-        if color == nil do
-          ""
-        else
-          Enum.at(color, 1)
-        end
-      end)
-
-    if color_list == [""] do
-      []
-    else
-      color_list
-    end
-
-    #    Regex.run(~r/^\d+ (.+bag)/, "2 shiny gold bags ")
-  end
-
-  def find_color_rule_line(color, lines) do
-    Enum.filter(lines, fn x_line ->
-      {:ok, reg} = Regex.compile("^" <> color)
-      String.match?(x_line, reg)
-    end)
-    |> List.first()
-  end
-end
+#defmodule Puzzle7Part1 do
+#  @moduledoc """
+#  Documentation for `Puzzle`.
+#  """
+#
+#  def contain_bag_directly(line, color) do
+#    {:ok, reg} = Regex.compile("^(?!" <> color <> ")(.*)" <> color)
+#    String.match?(line, reg)
+#  end
+#
+#  def contain_bag(line, color, [_ | _] = all_lines) do
+#    #    IO.inspect(line, label: "line", pretty: true)
+#
+#    if contain_bag_directly(line, color) do
+#      #      IO.inspect(color, label: "contain_bag_directly", pretty: true)
+#      true
+#    else
+#      #       找到 line 包含的所有儿子 ，check儿子是否包含color
+#      extract_contained_colors(line)
+#      |> IO.inspect(label: "extract_contained_colors", pretty: true)
+#      |> Enum.map(fn x_color -> find_color_rule_line(x_color, all_lines) end)
+#      |> IO.inspect(label: "find_color_rule_line ", pretty: true)
+#      |> Enum.any?(fn x_line -> contain_bag(x_line, color, all_lines) end)
+#    end
+#  end
+#
+#  def count_contain_bag([_ | _] = lines, color) do
+#    Enum.filter(lines, fn x_line -> contain_bag(x_line, color, lines) end)
+#    #    |> IO.inspect(label: "count_contain_bag", pretty: true)
+#    |> Enum.count()
+#  end
+#
+#  def count_contain_bag(file_path, color) do
+#    {:ok, file_content} = File.read(file_path)
+#
+#    String.split(file_content, "\n")
+#    |> count_contain_bag(color)
+#  end
+#
+#  def extract_contained_colors(line) do
+#    [_head | tail] = Regex.split(~r{(contain|,|\.)}, line, trim: true)
+#
+#    color_list =
+#      tail
+#      #      |> IO.inspect(label: "extract_contained_colors1", pretty: true)
+#      |> Enum.map(fn x ->
+#        color = Regex.run(~r/\d+ (.+bag)/, x)
+#        #          |> IO.inspect(label: "extract_contained_colors2", pretty: true)
+#
+#        if color == nil do
+#          ""
+#        else
+#          Enum.at(color, 1)
+#        end
+#      end)
+#
+#    if color_list == [""] do
+#      []
+#    else
+#      color_list
+#    end
+#
+#    #    Regex.run(~r/^\d+ (.+bag)/, "2 shiny gold bags ")
+#  end
+#
+#  def find_color_rule_line(color, lines) do
+#    Enum.filter(lines, fn x_line ->
+#      {:ok, reg} = Regex.compile("^" <> color)
+#      String.match?(x_line, reg)
+#    end)
+#    |> List.first()
+#  end
+#end
--- puzzle/test/puzzle7Part1_test.exs
+++ puzzle/test/puzzle7Part1_test.exs
@@ -1,149 +1,149 @@
-defmodule Puzzle7Part1Test do
-  use ExUnit.Case
-  doctest Puzzle7Part1
-
-  @find_color_bag "shiny gold bag"
-  @lines [
-    "light red bags contain 1 bright white bag, 2 muted yellow bags.",
-    "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
-    "bright white bags contain 1 shiny gold bag.",
-    "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
-    "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
-    "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
-    "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
-    "faded blue bags contain no other bags.",
-    "dotted black bags contain no other bags."
-  ]
-
-  test "直接包含@find_color_bag" do
-    assert Puzzle7Part1.contain_bag_directly(
-             "bright white bags contain 1 shiny gold bag.",
-             @find_color_bag
-           ) == true
-
-    assert Puzzle7Part1.contain_bag_directly(
-             "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
-             @find_color_bag
-           ) == true
-
-    assert Puzzle7Part1.contain_bag_directly(
-             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
-             @find_color_bag
-           ) == false
-  end
-
-  test "直接包含@find_color_bag, 另一种实现方式" do
-    assert Puzzle7Part1.contain_bag(
-             "bright white bags contain 1 shiny gold bag.",
-             @find_color_bag,
-             @lines
-           ) == true
-
-    assert Puzzle7Part1.contain_bag(
-             "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
-             @find_color_bag,
-             @lines
-           ) == true
-  end
-
-  test "A 包含B，B包含@find_color_bag， 则A包含@find_color_bag" do
-    assert Puzzle7Part1.contain_bag(
-             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
-             @find_color_bag,
-             @lines
-           ) == true
-
-    assert Puzzle7Part1.contain_bag(
-             "light red bags contain 1 bright white bag, 2 muted yellow bags.",
-             @find_color_bag,
-             @lines
-           ) == true
-  end
-
-  test "A 包含B，B包含C, C包含@find_color_bag， 则A包含@find_color_bag" do
-    line1 = "jenny1 bags contain 1 dark orange bag."
-    line2 = "jenny2 bags contain 2 light red bags."
-    lines = @lines ++ [line1, line2]
-
-    assert Puzzle7Part1.contain_bag(
-             line1,
-             @find_color_bag,
-             lines
-           ) == true
-
-    assert Puzzle7Part1.contain_bag(
-             line2,
-             @find_color_bag,
-             lines
-           ) == true
-  end
-
-  test "A 包含B，B包含C, C包含D，D包含@find_color_bag， 则A包含@find_color_bag" do
-    line1 = "jenny1 bags contain 1 dark orange bag."
-    line2 = "jenny2 bags contain 2 light red bags."
-    line3 = "jenny3 bags contain 2 jenny1 bags."
-    line4 = "jenny4 bags contain 2 jenny2 bags."
-    lines = @lines ++ [line1, line2]
-
-    assert Puzzle7Part1.contain_bag(
-             line3,
-             @find_color_bag,
-             lines
-           ) == true
-
-    assert Puzzle7Part1.contain_bag(
-             line4,
-             @find_color_bag,
-             lines
-           ) == true
-  end
-
-  test "所有直接or间接包含了某color bag的所有行，求行数：传入line list" do
-    assert Puzzle7Part1.count_contain_bag(@lines, @find_color_bag) == 4
-  end
-
-  test "所有直接or间接包含了某color bag的所有行，求行数：传入 input_sample.txt" do
-    assert Puzzle7Part1.count_contain_bag(
-             "/data/homework/puzzle/lib/puzzle7_input_sample.txt",
-             @find_color_bag
-           ) == 4
-  end
-
-  @tag timeout: 600_000
-  test "所有直接or间接包含了某color bag的所有行，求行数：传入 input.txt" do
-    assert Puzzle7Part1.count_contain_bag(
-             "/data/homework/puzzle/lib/puzzle7_input.txt",
-             @find_color_bag
-           ) == 235
-  end
-
-  test "根据color 找到 color到定义行" do
-    assert Puzzle7Part1.find_color_rule_line(@find_color_bag, @lines) ==
-             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
-
-    assert Puzzle7Part1.find_color_rule_line("dark olive bag", @lines) ==
-             "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
-
-    assert Puzzle7Part1.find_color_rule_line("faded blue bag", @lines) ==
-             "faded blue bags contain no other bags."
-  end
-
-  test "在line中，提取包含的 儿子bag" do
-    assert Puzzle7Part1.extract_contained_colors(
-             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
-           ) == ["dark olive bag", "vibrant plum bag"]
-
-    assert Puzzle7Part1.extract_contained_colors(
-             "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
-           ) == ["faded blue bag", "dotted black bag"]
-
-    assert Puzzle7Part1.extract_contained_colors(
-             "clear chartreuse bags contain 5 striped maroon bags, 5 light chartreuse bags, 4 drab black bags."
-           ) == ["striped maroon bag", "light chartreuse bag", "drab black bag"]
-
-    assert Puzzle7Part1.extract_contained_colors("dotted blue bags contain 4 clear cyan bags.") ==
-             ["clear cyan bag"]
-
-    assert Puzzle7Part1.extract_contained_colors("faded blue bags contain no other bags.") == []
-  end
-end
+#defmodule Puzzle7Part1Test do
+#  use ExUnit.Case
+#  doctest Puzzle7Part1
+#
+#  @find_color_bag "shiny gold bag"
+#  @lines [
+#    "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+#    "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+#    "bright white bags contain 1 shiny gold bag.",
+#    "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+#    "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+#    "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+#    "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+#    "faded blue bags contain no other bags.",
+#    "dotted black bags contain no other bags."
+#  ]
+#
+#  test "直接包含@find_color_bag" do
+#    assert Puzzle7Part1.contain_bag_directly(
+#             "bright white bags contain 1 shiny gold bag.",
+#             @find_color_bag
+#           ) == true
+#
+#    assert Puzzle7Part1.contain_bag_directly(
+#             "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+#             @find_color_bag
+#           ) == true
+#
+#    assert Puzzle7Part1.contain_bag_directly(
+#             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+#             @find_color_bag
+#           ) == false
+#  end
+#
+#  test "直接包含@find_color_bag, 另一种实现方式" do
+#    assert Puzzle7Part1.contain_bag(
+#             "bright white bags contain 1 shiny gold bag.",
+#             @find_color_bag,
+#             @lines
+#           ) == true
+#
+#    assert Puzzle7Part1.contain_bag(
+#             "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+#             @find_color_bag,
+#             @lines
+#           ) == true
+#  end
+#
+#  test "A 包含B，B包含@find_color_bag， 则A包含@find_color_bag" do
+#    assert Puzzle7Part1.contain_bag(
+#             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+#             @find_color_bag,
+#             @lines
+#           ) == true
+#
+#    assert Puzzle7Part1.contain_bag(
+#             "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+#             @find_color_bag,
+#             @lines
+#           ) == true
+#  end
+#
+#  test "A 包含B，B包含C, C包含@find_color_bag， 则A包含@find_color_bag" do
+#    line1 = "jenny1 bags contain 1 dark orange bag."
+#    line2 = "jenny2 bags contain 2 light red bags."
+#    lines = @lines ++ [line1, line2]
+#
+#    assert Puzzle7Part1.contain_bag(
+#             line1,
+#             @find_color_bag,
+#             lines
+#           ) == true
+#
+#    assert Puzzle7Part1.contain_bag(
+#             line2,
+#             @find_color_bag,
+#             lines
+#           ) == true
+#  end
+#
+#  test "A 包含B，B包含C, C包含D，D包含@find_color_bag， 则A包含@find_color_bag" do
+#    line1 = "jenny1 bags contain 1 dark orange bag."
+#    line2 = "jenny2 bags contain 2 light red bags."
+#    line3 = "jenny3 bags contain 2 jenny1 bags."
+#    line4 = "jenny4 bags contain 2 jenny2 bags."
+#    lines = @lines ++ [line1, line2]
+#
+#    assert Puzzle7Part1.contain_bag(
+#             line3,
+#             @find_color_bag,
+#             lines
+#           ) == true
+#
+#    assert Puzzle7Part1.contain_bag(
+#             line4,
+#             @find_color_bag,
+#             lines
+#           ) == true
+#  end
+#
+#  test "所有直接or间接包含了某color bag的所有行，求行数：传入line list" do
+#    assert Puzzle7Part1.count_contain_bag(@lines, @find_color_bag) == 4
+#  end
+#
+#  test "所有直接or间接包含了某color bag的所有行，求行数：传入 input_sample.txt" do
+#    assert Puzzle7Part1.count_contain_bag(
+#             "/data/homework/puzzle/lib/puzzle7_input_sample.txt",
+#             @find_color_bag
+#           ) == 4
+#  end
+#
+#  @tag timeout: 600_000
+#  test "所有直接or间接包含了某color bag的所有行，求行数：传入 input.txt" do
+#    assert Puzzle7Part1.count_contain_bag(
+#             "/data/homework/puzzle/lib/puzzle7_input.txt",
+#             @find_color_bag
+#           ) == 235
+#  end
+#
+#  test "根据color 找到 color到定义行" do
+#    assert Puzzle7Part1.find_color_rule_line(@find_color_bag, @lines) ==
+#             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
+#
+#    assert Puzzle7Part1.find_color_rule_line("dark olive bag", @lines) ==
+#             "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
+#
+#    assert Puzzle7Part1.find_color_rule_line("faded blue bag", @lines) ==
+#             "faded blue bags contain no other bags."
+#  end
+#
+#  test "在line中，提取包含的 儿子bag" do
+#    assert Puzzle7Part1.extract_contained_colors(
+#             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
+#           ) == ["dark olive bag", "vibrant plum bag"]
+#
+#    assert Puzzle7Part1.extract_contained_colors(
+#             "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
+#           ) == ["faded blue bag", "dotted black bag"]
+#
+#    assert Puzzle7Part1.extract_contained_colors(
+#             "clear chartreuse bags contain 5 striped maroon bags, 5 light chartreuse bags, 4 drab black bags."
+#           ) == ["striped maroon bag", "light chartreuse bag", "drab black bag"]
+#
+#    assert Puzzle7Part1.extract_contained_colors("dotted blue bags contain 4 clear cyan bags.") ==
+#             ["clear cyan bag"]
+#
+#    assert Puzzle7Part1.extract_contained_colors("faded blue bags contain no other bags.") == []
+#  end
+#end
