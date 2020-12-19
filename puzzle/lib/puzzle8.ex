dc3e9b6830b9accacb79dab55bf7c25c97ae57cd
huzeli <jenny.hu@scrumcn.com>
2020-12-19 20:42:20 +0800

    day8part1 优化

--- puzzle/lib/puzzle8.ex
+++ puzzle/lib/puzzle8.ex
@@ -3,62 +3,42 @@ defmodule Puzzle8 do
   Documentation for `Puzzle`.
   """
 
-  def acc(accumulator, line_index, argument) do
-    %{
-      accumulator: accumulator + argument,
-      line_index: line_index
-    }
+  def run_all_instruction(file_path) do
+    lines = FileTool.convert_file_to_list(file_path)
+    run_all_instruction(0, [], 0, lines)
   end
 
-  def jmp(accumulator, line_index, argument) do
-    %{
-      accumulator: accumulator,
-      line_index: line_index + argument
-    }
+  def run_all_instruction(accumulator, trace, line_index, lines) do
+    run_result = extract_instruction(lines, line_index) |> run_one_instruction(accumulator, trace)
+
+    if Enum.any?(trace, fn x -> x == run_result.line_index end) do
+      {:infinite_loop, run_result}
+    else
+      run_all_instruction(run_result.accumulator, run_result.trace, run_result.line_index, lines)
+    end
   end
 
-  def nop(accumulator, line_index, argument) do
-    %{
-      accumulator: accumulator,
-      line_index: line_index + 1
-    }
-  end
-
-  def extract_instruction(line_content_string) do
-    line_content = String.split(line_content_string)
-    %{operation: List.first(line_content), argument: String.to_integer(List.last(line_content))}
-  end
-
-  def extract_instruction(line_content_string, line_index) do
-    line_content = String.split(line_content_string)
+  def extract_instruction(lines, line_index) do
+    line_content = Enum.at(lines, line_index) |> String.split()
 
     %{operation: List.first(line_content), argument: String.to_integer(List.last(line_content))}
     |> Map.put(:line_index, line_index)
   end
 
-  def run_instruction(file_path) do
-    lines = FileTool.convert_file_to_list(file_path)
-    run_instruction(0, [], 0, lines)
-  end
-
-  def run_instruction(accumulator, trace, line_index, lines) do
-    IO.inspect(lines)
-    IO.inspect(line_index)
-
-    %{line_index: line_index, operation: operation, argument: argument} =
-      extract_instruction(Enum.at(lines, line_index), line_index)
-
-    run_result = %{
+  def run_one_instruction(
+        %{
+          line_index: line_index,
+          operation: operation,
+          argument: argument
+        },
+        accumulator,
+        trace
+      ) do
+    %{
       accumulator: change_accumulator(operation, argument, accumulator),
       line_index: change_line_index(operation, argument, line_index),
       trace: trace ++ [line_index]
     }
-
-    if Enum.any?(trace, fn x -> x == run_result.line_index end) do
-      {:infinite_loop, run_result}
-    else
-      run_instruction(run_result.accumulator, run_result.trace, run_result.line_index, lines)
-    end
   end
 
   def change_accumulator(operation, argument, accumulator) do
