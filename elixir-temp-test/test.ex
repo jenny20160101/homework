1eff72be413016a184459d54e63552534ff0c829
jenny <jenny.hu@scrumcn.com>
2021-05-14 10:23:14 +0800

    新增测试项目

---
+++ elixir-temp-test/test.ex
@@ -0,0 +1,22 @@
+defmodule Number do
+    def new(string), do: Integer.parse(string)|> elem(0)
+
+    def add(number, addend), do: number + addend
+
+    def to_string(number), do:
+    Integer.to_string(number)
+
+end
+
+defmodule Test do
+  def test1 do
+
+    list=[1,2,3]
+    total=Number.new("0")
+    reducer=&Number.add(&2,&1)
+    converter=&Number.to_string
+    Enum.reduce(list, total, reducer)|> converter.()
+
+  end
+
+end
