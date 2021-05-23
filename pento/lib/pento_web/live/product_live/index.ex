d0d54d980a8aebf91c6056acfc245828e648aeb0
jenny <jenny.hu@scrumcn.com>
2021-05-23 15:11:27 +0800

    comment

--- pento/lib/pento_web/live/product_live/index.ex
+++ pento/lib/pento_web/live/product_live/index.ex
@@ -15,6 +15,9 @@ defmodule PentoWeb.ProductLive.Index do
 
   @impl true
   def handle_params(params, _url, socket) do
+    # IO.inspect(params, label: "params:", pretty: true)
+    # IO.inspect(_url, label: "_url:", pretty: true)
+    # IO.inspect(socket, label: "socket:", pretty: true)
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
