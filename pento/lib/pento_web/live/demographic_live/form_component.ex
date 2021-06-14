5f494d7372b6c8648aba55ba0b93089879e17ec0
huzeli <jenny.hu@scrumcn.com>
2021-06-15 07:02:35 +0800

    stateful component

--- pento/lib/pento_web/live/demographic_live/form_component.ex
+++ pento/lib/pento_web/live/demographic_live/form_component.ex
@@ -19,4 +19,23 @@ defmodule PentoWeb.DemographicLive.FormComponent do
   def assign_changeset(%{assigns: %{demographic: demographic}} = socket) do
     assign(socket, :changeset, Survey.change_demographic(demographic))
   end
+
+  def handle_event("save", %{"demographic" => demographic_params}, socket) do
+    IO.puts("Handling save event and saving demographic record...")
+    IO.inspect(demographic_params)
+    # {:noreply, socket}
+
+    {:noreply, save_demographic(socket, demographic_params)}
+  end
+
+  def save_demographic(socket, demographic_params) do
+    case Survey.create_demographic(demographic_params) do
+      {:ok, demographic} ->
+        send(self(), {:created_demographic, demographic})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign(socket, changeset: changeset)
+    end
+  end
 end
--- pento/lib/pento_web/live/survey_lie.ex
+++ pento/lib/pento_web/live/survey_lie.ex
@@ -4,7 +4,14 @@ defmodule PentoWeb.SurveyLive do
 
   @impl true
   def mount(_params, %{"user_token" => token} = _session, socket) do
-    {:ok, socket |> assign_user(token)}
+    {:ok,
+     socket
+     |> assign_user(token)
+     |> assing_demographic()}
+  end
+
+  def assing_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :deomgraphic, Survey.get_demographic_by_user(current_user))
   end
 
   def assign_user(socket, token) do
@@ -14,5 +21,16 @@ defmodule PentoWeb.SurveyLive do
     assign_new(socket, :current_user, fn ->
       Accounts.get_user_by_session_token(token)
     end)
+  end
+
+  @impl true
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
+  def handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographic created successfully")
+    |> assign(:demographic, demographic)
   end
 end
