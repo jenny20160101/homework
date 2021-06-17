fb11818d4f90368c93a19c7895a723c654dfd47c
huzeli <jenny.hu@scrumcn.com>
2021-06-18 07:05:44 +0800

    submit rating

--- pento/lib/pento_web/live/survey_live.ex
+++ pento/lib/pento_web/live/survey_live.ex
@@ -43,4 +43,18 @@ defmodule PentoWeb.SurveyLive do
   defp list_products(user) do
     Catalog.list_products_with_user_ratings(user)
   end
+
+  def handle_info({:created_rating, updated_product, product_index}, socket) do
+    {:noreply, handle_rating_created(socket, updated_product, product_index)}
+  end
+
+  def handle_rating_created(
+        %{assigns: %{products: products}} = socket,
+        updated_product,
+        product_index
+      ) do
+    socket
+    |> put_flash(:info, "Rating submitted succeedfully")
+    |> assign(:products, List.replace_at(products, product_index, updated_product))
+  end
 end
