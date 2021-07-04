10f49931d68a263a546b31ccce33d29b9849d3d2
jenny <jenny.hu@scrumcn.com>
2021-07-04 15:14:03 +0800

    new project

---
+++ my-vue3-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h2> My Vue 3 Project</h2>
+    <div id="nav" class="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent } from "vue"
+
+export default defineComponent ({
+  name: 'App'
+})
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.rout-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ my-vue3-project/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <ItemsListComponent :items="items" />
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from  'vue'
+import ItemsListComponent from "@/components/items/ItemsList.componet.vue"
+
+export default defineComponent({
+    name: "Home",
+    components:{ItemsListComponent},
+    setup() {
+
+        const items: any[] = [
+            {id:1, name: "Item 1"},
+            {id:2, name: "Item 2"},
+            {id:3, name: "Item 3"},
+        ]
+        return {items}
+    }
+})
+</script>
