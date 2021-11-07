79f0124d65a1dd962d9062db00002fa3ed967db6
huzeli <jenny.hu@scrumcn.com>
2021-11-07 17:22:47 +0800

    添加上传组件

--- new_pento1031/pento/lib/pento_web/live/product_live/form_component.ex
+++ new_pento1031/pento/lib/pento_web/live/product_live/form_component.ex
@@ -10,7 +10,48 @@ defmodule PentoWeb.ProductLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(
+       :image,
+       accept: ~w(.jpg .jpeg .png),
+       max_entries: 1,
+       auto_upload: true,
+       progress: &handle_progress/3
+     )}
+  end
+
+  def handle_progress(:image, entry, socket) do
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+
+      {:noreply,
+       socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> update_changeset(:image_upload, path)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def update_changeset(%{assigns: %{changeset: changeset}} = socket, key, value) do
+    socket
+    |> assign(:changeset, Ecto.Changeset.put_change(changeset, key, value))
+  end
+
+  def upload_static_file(%{path: path}, socket) do
+    dest =
+      Path.join(
+        "priv/static/images",
+        Path.basename(path)
+      )
+
+    File.cp!(path, dest)
+    Routes.static_path(socket, "/images/#{Path.base() - name(dest)}")
   end
 
   @impl true
